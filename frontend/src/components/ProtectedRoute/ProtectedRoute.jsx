569db37f2d1b5fc13127743592b46ad61e104acc
Lucero Uzuriaga <luuzuriaga@gmail.com>
2025-10-01 05:00:49 +0000

    chore: eliminar carpeta frontend

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-//ProtectedRoute.jsx
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ loggedIn, component: Component, ...props }) => {
-  return loggedIn ? <Component {...props} /> : <Navigate to="/signin" replace />;
-};
-
-export default ProtectedRoute;
