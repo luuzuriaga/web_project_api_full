66c87f2a1a671a4af0f71657d68f66242029375a
Lucero Uzuriaga <luuzuriaga@gmail.com>
2025-10-21 02:47:22 +0000

    cambios fronted allow all ips

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, transformWithEsbuild } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+    host: true,
   },
 });
